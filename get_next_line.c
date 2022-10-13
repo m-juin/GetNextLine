2b0cffd070d1dabdf9fdf56964dbd3a6cba1b37a
mjuin <mjuin@student.42.fr>
2022-10-14 00:19:36 +0200

    one by one

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mjuin <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 21:56:21 by mjuin             #+#    #+#             */
-/*   Updated: 2022/10/12 23:55:39 by mjuin            ###   ########.fr       */
+/*   Updated: 2022/10/14 00:07:32 by mjuin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int	ft_findnextline(char *str)
-{
-	int	pos;
-
-	pos = 0;
-	while (str[pos] && str[pos] != '\n')
-	{
-		pos++;
-	}
-	return (pos);
-}
-
 void	ft_putstr(char *str)
 {
 	while (*str)
@@ -36,37 +24,33 @@ void	ft_putstr(char *str)
 
 char *get_next_line(int fd)
 {
-	static int	readed;
-	int			possave;
-	char		*buf;
-	char		*printed;
-	int			pos;
-	
-	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buf)
-		return (NULL);
-	possave = readed;
-	read(fd, buf, BUFFER_SIZE);
-	readed += ft_findnextline(buf);
-	printed = malloc(((readed - possave) + 1) * sizeof(char));
-	if(!printed)
-	{
-		free(buf);
-		return (NULL);
-	}
+	char 	*buffer;
+	char	tmp[1];
+	int		readvalue;
+	int	pos;
+
 	pos = 0;
-	while ((readed - (possave + pos)) != 0)
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if(!buffer)
+		return (NULL);
+	while (pos < BUFFER_SIZE)
 	{
-		printed[pos] = buf[pos];
+		readvalue = read(fd, tmp, 1);
+		if(readvalue == 0)
+			return (NULL);
+		buffer[pos] = tmp[0];
+		if (buffer[pos] == '\n')
+			break ;
 		pos++;
 	}
-	free(buf);
-	return (printed);
+	buffer[pos + 1] = 0;
+	return (buffer);
 }
 
 int	main(void)
 {
 	int	fd;
+	int	fd1;
 
 	fd = open("42", O_RDONLY);
 	if(fd == -1)
@@ -74,6 +58,16 @@ int	main(void)
 		ft_putstr("open() error");
 		return (0);
 	}
+	fd1 = open("43", O_RDONLY);
+	if(fd1 == -1)
+	{
+		ft_putstr("open() error");
+		return (0);
+	}
+
+	ft_putstr(get_next_line(fd));
+	ft_putstr(get_next_line(fd));
+	ft_putstr(get_next_line(fd));
 	ft_putstr(get_next_line(fd));
 	ft_putstr(get_next_line(fd));
 	return (0);
